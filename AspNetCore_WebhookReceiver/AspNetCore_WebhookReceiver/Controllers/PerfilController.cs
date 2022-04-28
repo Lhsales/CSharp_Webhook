3129d29390836e4812f03e3930fc478abe1af2ac
Lhsales <henri.luhsales@gmail.com>
2022-04-28 02:18:15 -0300

    Criação do PerfilController

---
+++ AspNetCore_WebhookReceiver/AspNetCore_WebhookReceiver/Controllers/PerfilController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Dominio.Models;
+using Dominio.Interfaces;
+using Servico.Services;
+
+namespace AspNetCore_WebhookReceiver.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private IBaseService<Webhook_perfil> _basePerfilService;
+        public PerfilController(IBaseService<Webhook_perfil> basePerfilService)
+        {
+            _basePerfilService = basePerfilService;
+        }
+
+
+        public IActionResult Index()
+        {
+            IList<Webhook_perfil> listaPerfis = _basePerfilService.GetAll();
+
+            return View();
+        }
+    }
+}
