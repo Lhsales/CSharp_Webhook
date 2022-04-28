0acc11aa574d513894397564ace5caa77ac41400
Lhsales <henri.luhsales@gmail.com>
2022-04-28 00:38:19 -0300

    Correções nos nomes das pastas

--- AspNetCore_WebhookReceiver/Infra/Migrations/20220427063456_Initial.Designer.cs
+++ AspNetCore_WebhookReceiver/Infra/Migrations/20220427063456_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Repositorio.FonteDeDados;
+using Infra.Contexts;
 
 namespace Repositorio.Migrations
 {
     [DbContext(typeof(PersisteContext))]
-    partial class PersisteContextModelSnapshot : ModelSnapshot
+    [Migration("20220427063456_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AspNetCore_WebhookReceiver/Infra/Migrations/PersisteContextModelSnapshot.cs
+++ AspNetCore_WebhookReceiver/Infra/Migrations/PersisteContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Repositorio.FonteDeDados;
+using Infra.Contexts;
 
 namespace Repositorio.Migrations
 {
     [DbContext(typeof(PersisteContext))]
-    [Migration("20220427063456_Initial")]
-    partial class Initial
+    partial class PersisteContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
