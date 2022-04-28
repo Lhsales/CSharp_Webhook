80aa246aa582edfba09a7e98bf13b66ca7047f39
Lhsales <henri.luhsales@gmail.com>
2022-04-28 00:18:52 -0300

    Ajustes Repositório e Aplicação

---
+++ AspNetCore_WebhookReceiver/Projeto.Dominio/Interfaces/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dominio.Models;
+
+namespace Dominio.Interfaces
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add(TEntity obj);
+        void Delete(int id);
+        IList<TEntity> GetAll();
+        TEntity GetById(int id);
+        TEntity Update(TEntity obj);
+
+    }
+}
